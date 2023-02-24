3aafa7f8c51eea37f570608c789b3671044f74d7
Dmytro Soroka <dmytro.soroka@betterme.world>
2023-02-24 18:16:05 +0200

    start to creation of LinkedList

---
+++ data-structures/linked-list.js
@@ -0,0 +1,23 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor(value) {
+    const newNode = new Node(value);
+    this.head = newNode;
+    this.tail = this.head;
+    this.length = 1;
+  }
+
+  push(value) {}
+
+  unshift(value) {}
+
+  insert(index, value) {}
+}
+
+const linkedList = new LinkedList(4);
