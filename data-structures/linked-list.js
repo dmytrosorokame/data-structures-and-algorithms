0b268b5fbefb3482e98f85904e2b6e3045aaa754
Dmytro Soroka <dmytro.soroka@betterme.world>
2023-02-24 19:25:04 +0200

    add linkedList insert method

--- data-structures/linked-list.js
+++ data-structures/linked-list.js
@@ -106,5 +106,19 @@ class LinkedList {
     return false;
   }
 
-  insert(index, value) {}
+  insert(index, value) {
+    if (index === 0) return this.unshift(value);
+    if (index === this.length) return this.push(value);
+    if (index < 0 || index > this.length) return false;
+
+    const newNode = new Node(value);
+    const temp = this.get(index - 1);
+
+    newNode.next = temp.next;
+    temp.next = newNode;
+
+    this.length++;
+
+    return true;
+  }
 }
