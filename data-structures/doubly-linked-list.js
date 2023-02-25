ecc21520f0feed573155fd69e29fe96d6057abb4
Dmytro Soroka <dmytro.soroka@betterme.world>
2023-02-25 20:24:49 +0200

    add doublyLinkedList insert method

--- data-structures/doubly-linked-list.js
+++ data-structures/doubly-linked-list.js
@@ -114,4 +114,24 @@ class DoublyLinkedList {
 
     return false;
   }
+
+  insert(index, value) {
+    if (index === 0) return this.unshift(value);
+    if (index === this.length) return this.push(value);
+    if (index < 0 || index > this.length) return false;
+
+    const newNode = new Node(value);
+
+    const before = this.get(index - 1);
+    const after = before.next;
+
+    newNode.prev = before;
+    newNode.next = after;
+    before.next = newNode;
+    after.prev = newNode;
+
+    this.length++;
+
+    return true;
+  }
 }
