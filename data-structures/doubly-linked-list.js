03334fe97caa42803e69ff24a9b34c6e43f4cf91
Dmytro Soroka <dmytro.soroka@betterme.world>
2023-02-25 11:02:28 +0200

    add doublyLinkedList constructor

---
+++ data-structures/doubly-linked-list.js
@@ -0,0 +1,16 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor(value) {
+    const newNode = new Node(value);
+    this.head = newNode;
+    this.tail = newNode;
+    this.length = 1;
+  }
+}
