0e55942da23b53e2411cce3ab9554d9f8cb10118
Dmytro Soroka <dmytro.soroka@betterme.world>
2023-02-26 13:07:01 +0200

    create queue constructor

---
+++ data-structures/queue.js
@@ -0,0 +1,15 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor(value) {
+    const newNode = new Node(value);
+    this.first = newNode;
+    this.last = newNode;
+    this.length = 1;
+  }
+}
