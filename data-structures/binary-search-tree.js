7be601866abea7e6b00d7a766b337e9203e86f3d
Dmytro Soroka <dmytro.soroka@betterme.world>
2023-02-26 16:58:58 +0200

    add binarySearchTree contains method

--- data-structures/binary-search-tree.js
+++ data-structures/binary-search-tree.js
@@ -41,4 +41,22 @@ class BinarySearchTree {
       }
     }
   }
+
+  contains(value) {
+    if (!this.root) return false;
+
+    let temp = this.root;
+
+    while (temp) {
+      if (value > temp.value) {
+        temp = temp.right;
+      } else if (value < temp.value) {
+        temp = temp.left;
+      } else {
+        return true;
+      }
+    }
+
+    return false;
+  }
 }
