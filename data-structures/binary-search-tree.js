c8cdd106d34a2832fdfa925ee5b086b55746f842
Dmytro Soroka <dmytro.soroka@betterme.world>
2023-02-26 16:50:05 +0200

    add binarySearchTree insert method

--- data-structures/binary-search-tree.js
+++ data-structures/binary-search-tree.js
@@ -10,4 +10,35 @@ class BinarySearchTree {
   constructor() {
     this.root = null;
   }
+
+  insert(value) {
+    const newNode = new Node(value);
+
+    if (!this.root) {
+      this.root = newNode;
+      return this;
+    }
+
+    let temp = this.root;
+
+    while (true) {
+      if (newNode.value === temp.value) return;
+
+      if (value > temp.value) {
+        if (!temp.right) {
+          temp.right = newNode;
+          return this;
+        }
+
+        temp = temp.right;
+      } else {
+        if (!temp.left) {
+          temp.left = newNode;
+          return this;
+        }
+
+        temp = temp.left;
+      }
+    }
+  }
 }
