faf4c85473e653bbf50f69d73838f64d0129a2d5
Dmytro Soroka <dmytro.soroka@betterme.world>
2023-02-26 10:43:05 +0200

    create Stack constructor

---
+++ data-structures/stack.js
@@ -0,0 +1,14 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor(value) {
+    const newNode = new Node(value);
+    this.top = newNode;
+    this.length = 1;
+  }
+}
