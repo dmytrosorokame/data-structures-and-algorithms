3ec5e91feffcd4c977ebc6dd81ee9bfad7d44a7d
Dmytro Soroka <dmytro.soroka@betterme.world>
2024-03-25 17:46:36 +0100

    feat: added hash table get method

--- data-structures/hash-table.js
+++ data-structures/hash-table.js
@@ -24,16 +24,36 @@ class HashTable {
 
     return this;
   }
+
+  get(key) {
+    const index = this._hash(key);
+
+    const cell = this.dataMap[index];
+
+    if (cell) {
+      for (let i = 0; i < cell.length; i++) {
+        const [elKey, elValue] = cell[i];
+
+        if (elKey === key) {
+          return elValue;
+        }
+      }
+    }
+
+    return undefined;
+  }
 }
 
 const hashTable = new HashTable();
 hashTable.set("myTest1", 98);
-hashTable.set("myTest2", 98);
-hashTable.set("myTest3", 98);
-hashTable.set("myTest4", 98);
-hashTable.set("myTest5", 98);
-hashTable.set("myTest6", 98);
-hashTable.set("myTest7", 98);
-hashTable.set("myTest8", 98);
+hashTable.set("myTest2", 983);
+hashTable.set("myTest3", 981);
+hashTable.set("myTest4", 985);
+hashTable.set("myTest5", 989);
+hashTable.set("myTest6", 93);
+hashTable.set("myTest7", 18);
+hashTable.set("myTest8", 68);
 
 console.log(JSON.stringify(hashTable));
+
+console.log(hashTable.get("myTest1"));
