186825ffd5137615040b4f4c6964ffe158c698cf
Dmytro Soroka <dmytro.soroka@betterme.world>
2024-03-28 17:28:35 +0100

    feat: added addEdge method to graph

--- data-structures/graph.js
+++ data-structures/graph.js
@@ -11,4 +11,15 @@ class Graph {
 
     return false;
   }
+
+  addEdge(vertex1, vertex2) {
+    if (this.adjacencyList[vertex1] && this.adjacencyList[vertex2]) {
+      this.adjacencyList[vertex1].push(vertex2);
+      this.adjacencyList[vertex2].push(vertex1);
+
+      return true;
+    }
+
+    return false;
+  }
 }
