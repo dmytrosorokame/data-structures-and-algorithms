f21006d75d7113452a7aefda848265f91f4ba23a
Dmytro Soroka <dmytro.soroka@betterme.world>
2024-04-05 17:34:55 +0200

    feat: added helper methods for Heap

---
+++ data-structures/heap.js
@@ -0,0 +1,28 @@
+class Heap {
+  #heap = [];
+
+  constructor() {}
+
+  getHeap() {
+    return [...this.#heap];
+  }
+
+  #leftChild(index) {
+    return 2 * index + 1;
+  }
+
+  #rightChild(index) {
+    return 2 * index + 2;
+  }
+
+  #parent(index) {
+    return Math.floor((index - 1) / 2);
+  }
+
+  #swap(index1, index2) {
+    [this.#heap[index1], this.#heap[index2]] = [
+      this.#heap[index2],
+      this.#heap[index1],
+    ];
+  }
+}
