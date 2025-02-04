eab6fa8e06cc35df7cf3f3ef6451134d10b0bed5
Dmytro Soroka <dmytro.soroka@betterme.world>
2025-02-04 16:44:21 +0100

    feat: added bubble sort

---
+++ algorithms/bubbleSort.js
@@ -0,0 +1,15 @@
+function bubbleSort(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    for (let j = 0; j < i; j++) {
+      if (array[j] > array[j + 1]) {
+        const temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
+      }
+    }
+  }
+
+  return array;
+}
+
+console.log(bubbleSort([4, 2, 6, 5, 1, 3]));
