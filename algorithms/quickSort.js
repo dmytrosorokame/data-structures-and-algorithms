93b94a1f0705ab7a2d9ac58e9ebeab7da14be76b
Dmytro Soroka <dmytro.soroka@betterme.world>
2025-02-05 17:04:50 +0100

    feat: added quick sort

---
+++ algorithms/quickSort.js
@@ -0,0 +1,34 @@
+function swap(array, firstIndex, secondIndex) {
+  const temp = array[firstIndex];
+  array[firstIndex] = array[secondIndex];
+  array[secondIndex] = temp;
+}
+
+function pivot(array, pivotIndex = 0, endIndex = array.length - 1) {
+  let swapIndex = pivotIndex;
+
+  for (let i = pivotIndex + 1; i < endIndex; i++) {
+    if (array[i] < array[pivotIndex]) {
+      swapIndex++;
+      swap(array, swapIndex, i);
+    }
+  }
+
+  swap(array, pivotIndex, swapIndex);
+
+  return swapIndex;
+}
+
+function quickSort(array, left = 0, right = array.length - 1) {
+  if (left < right) {
+    const pivotIndex = pivot(array, left, right);
+    quickSort(array, left, pivotIndex - 1);
+    quickSort(array, pivotIndex + 1, right);
+  }
+
+  return array;
+}
+
+const array = [4, 6, 1, 7, 3, 2, 5];
+
+console.log(quickSort(array));
