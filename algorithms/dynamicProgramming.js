e872220e90e72f2bf084ed1feee8b500d6fdb0df
Dmytro Soroka <dmytro.soroka@betterme.world>
2025-02-06 16:14:32 +0100

    feat: added bottomUp DP solution

--- algorithms/dynamicProgramming.js
+++ algorithms/dynamicProgramming.js
@@ -20,3 +20,18 @@ function fibonacci(n) {
 
 console.log(fibonacci(40));
 console.log("Counter", counter);
+
+function fibonacciBottomUp(n) {
+  const fibArray = [];
+
+  fibArray[0] = 0;
+  fibArray[1] = 1;
+
+  for (let i = 2; i <= n; i++) {
+    fibArray[i] = fibArray[i - 1] + fibArray[i - 2];
+  }
+
+  return fibArray[n];
+}
+
+console.log(fibonacciBottomUp(40));
